55b89a0e9aef464f652356c15c25bebe4b18f1ef
JAMEEL ALEDLBI <JAMEL_056@hotmail.com>
2022-02-25 17:26:50 +0300

    Add project files.

---
+++ CommanderGQL/GraphQL/Query.cs
@@ -0,0 +1,17 @@
+﻿using CommanderGQL.Data;
+using CommanderGQL.Models;
+using HotChocolate;
+using HotChocolate.Data;
+using System.Linq;
+
+namespace CommanderGQL.GraphQL
+{
+    public class Query
+    {
+        [UseDbContext(typeof(AppDbContext))]
+        public IQueryable<Platform> GetPlatform([ScopedService] AppDbContext context)
+        {
+            return context.Platforms;
+        }
+    }
+}
---
+++ CommanderGQL/Startup.cs
@@ -0,0 +1,51 @@
+using CommanderGQL.Data;
+using CommanderGQL.GraphQL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommanderGQL
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddPooledDbContextFactory<AppDbContext>(options => {
+                options.UseSqlServer(_configuration.GetConnectionString("CommandConStr"));
+            });
+
+            services.AddGraphQLServer()
+                .AddQueryType<Query>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+            });
+        }
+    }
+}
